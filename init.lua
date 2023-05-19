b9ec4b41889ced9158f71668ad8c77557f676c51
Jeremy Reeder <jeremy.reeder@pm.me>
2023-05-19 10:12:03 -0600

    Added hotkeys

--- init.lua
+++ init.lua
@@ -1,24 +1,34 @@
 -- welcome
 hs.alert.show('Hammerspoon')
 
--- reload
+-- fill screen by default
+hs.window.filter.default:subscribe(hs.window.filter.windowCreated, function() hs.window.focusedWindow():moveToUnit({0, 0, 1, 1}) end)
+
+-- hotkey to reload
 hs.hotkey.bind({'cmd', 'ctrl'}, 'r', hs.reload)
 
--- fill screen by default
-hs.window.filter.default:subscribe(hs.window.filter.windowCreated, function() hs.window.focusedWindow():movetoUnit({0, 0, 1, 1}) end)
-
--- fill screen
+-- hotkey to fill screen
 hs.hotkey.bind({'cmd','ctrl'}, 'space', function()
 	local window = hs.window.focusedWindow()
 	window:setFullScreen(false)
 	window:moveToUnit({0, 0, 1, 1})
 end)
 
--- fill left or right half of screen
+-- hotkeys to fill half of screen
 hs.hotkey.bind({'cmd', 'ctrl'}, 'h', function()
 	local window = hs.window.focusedWindow()
 	window:setFullScreen(false)
 	window:moveToUnit({0, 0, 0.5, 1})
+end)
+hs.hotkey.bind({'cmd', 'ctrl'}, 'j', function()
+	local window = hs.window.focusedWindow()
+	window:setFullScreen(false)
+	window:moveToUnit({0, 0.5, 1, 0.5})
+end)
+hs.hotkey.bind({'cmd', 'ctrl'}, 'k', function()
+	local window = hs.window.focusedWindow()
+	window:setFullScreen(false)
+	window:moveToUnit({0, 0, 1, 0.5})
 end)
 hs.hotkey.bind({'cmd', 'ctrl'}, 'l', function()
 	local window = hs.window.focusedWindow()
@@ -26,35 +36,33 @@ hs.hotkey.bind({'cmd', 'ctrl'}, 'l', function()
 	window:moveToUnit({0.5, 0, 0.5, 1})
 end)
 
--- fill top or bottom half of screen
-hs.hotkey.bind({'cmd', 'ctrl'}, 'k', function()
+-- hotkeys to move window to another screen
+hs.hotkey.bind({'cmd', 'ctrl', 'shift'}, 'h', function()
 	local window = hs.window.focusedWindow()
-	window:setFullScreen(false)
-	window:moveToUnit({0, 0, 1, 0.5})
-end)
-hs.hotkey.bind({'cmd', 'ctrl'}, 'j', function()
-	local window = hs.window.focusedWindow()
-	window:setFullScreen(false)
-	window:moveToUnit({0, 0.5, 1, 0.5})
-end)
-
--- move window to another screen
-hs.hotkey.bind({'cmd', 'ctrl', 'shift'}, 'k', function()
-	local window = hs.window.focusedWindow()
-	window:moveOneScreenNorth(false, true)
+	window:moveOneScreenWest(false, true)
 	window:screen():setMain()
 	window:focus()
-	window:moveToUnit({0, 0, 1, 1}) -- seems to have no effect
 end)
 hs.hotkey.bind({'cmd', 'ctrl', 'shift'}, 'j', function()
 	local window = hs.window.focusedWindow()
 	window:moveOneScreenSouth(false, true)
 	window:screen():setMain()
 	window:focus()
-	window:moveToUnit({0, 0, 1, 1}) -- seems to have no effect
+end)
+hs.hotkey.bind({'cmd', 'ctrl', 'shift'}, 'k', function()
+	local window = hs.window.focusedWindow()
+	window:moveOneScreenNorth(false, true)
+	window:screen():setMain()
+	window:focus()
+end)
+hs.hotkey.bind({'cmd', 'ctrl', 'shift'}, 'l', function()
+	local window = hs.window.focusedWindow()
+	window:moveOneScreenEast(false, true)
+	window:screen():setMain()
+	window:focus()
 end)
 
--- navigate windows directionally
+-- hotkeys to navigate windows directionally
 hs.hotkey.bind({'option'}, 'h', function()
 	local window = hs.window.focusedWindow()
 	window:focusWindowWest(window:application():allWindows())
@@ -72,14 +80,17 @@ hs.hotkey.bind({'option'}, 'l', function()
 	window:focusWindowEast(window:application():allWindows())
 end)
 
--- focus the most recently focused of the current application's other windows
+-- hotkey to focus the app's prior window
 hs.hotkey.bind({'option'}, 'tab', function() hs.window.focusedWindow():application():allWindows()[2]:focus() end)
 
--- cycle focus through the current application's windows
+-- hotkey to cycle thru the app's windows
 hs.hotkey.bind({'option', 'shift'}, 'tab', function()
 	local windows = hs.window.focusedWindow():application():allWindows()
 	windows[#windows]:focus()
 end)
 
--- launch a web browser
-hs.hotkey.bind({'cmd'}, '/', function() hs.execute('open https://duckduckgo.com') end)
+-- hotkey to launch a web browser
+hs.hotkey.bind({'cmd', 'ctrl'}, '/', function() hs.execute('open https://duckduckgo.com') end)
+
+-- hotkey to launch Finder
+hs.hotkey.bind({'cmd', 'ctrl'}, 'Return', function() hs.execute('open --reveal ~/Documents') end)
